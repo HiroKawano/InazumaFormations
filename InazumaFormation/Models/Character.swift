ed9c1e2d3c73837b8ddbefdef66f95f69ece45eb
Hiro Kawano <hk87492736@gmail.com>
2025-07-09 05:30:24 +0900

    Reorganization

---
+++ InazumaFormation/Models/Character.swift
@@ -0,0 +1,14 @@
+//
+//  Character.swift
+//  InazumaFormation
+//
+//  Created by Hiro Kawano.
+//
+import SwiftUI
+
+struct Character: Identifiable {
+    var id: UUID
+    var name: String
+    var position: CGPoint
+    var imageName: String 
+}
