8cda23a3dbb6fb5e085b61814ae8a917e46cac9f
Hiro Kawano <hk87492736@gmail.com>
2025-07-09 06:12:25 +0900

    Reorganization

---
+++ InazumaFormation/InazumaFormationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InazumaFormationApp.swift
+//  InazumaFormation
+//
+//  Created by Hiro Kawano.
+//
+
+import SwiftUI
+
+@main
+struct InazumaFormationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView() 
+        }
+    }
+}
